83b24e29966a8d4d96bc42f9cd4289ce154bf6cc
Cristian Cappo <ccappopy@gmail.com>
2026-03-05 15:59:17 -0300

    Agregar pseudocódigo

--- Clase03/clase3_ejercicio0.py
+++ Clase03/clase3_ejercicio0.py
@@ -1,7 +1,16 @@
 # Algoritmo - Sección MJ - 2026
 # -----------------------------
-# Script Python que lee tres variables enteras a, b y c
+# Algoritmo que lee tres variables enteras a, b y c
 # e imprime a**b**c
+
+# Pseudocódigo
+# var
+#   a,b,c,r  : numerico
+# inicio
+#   leer(a,b,c)
+#   r = a^b^c
+#   imprimir("El resultado es ", r)
+# fin
 
 a= int(input("El valor de a:"))
 b= int(input("El valor de b:"))
--- Clase03/clase3_ejercicio1.py
+++ Clase03/clase3_ejercicio1.py
@@ -3,6 +3,16 @@
 # Algoritmo que dado el radio de una esfera
 # imprime su volumen
 
+# En Pseudocódigo
+# var 
+#   r, v : numerico
+# const
+#   PI = 3.14
+# inicio
+#   leer(r)
+#   v = 4/3 * PI * r^3
+#   imprimir("El volumen es ", v)
+# fin 
 import math
 
 r = float(input('El radio es: '))
